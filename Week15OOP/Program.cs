7e3c08b70704e2c02baee35534c4745d33ed63d8
Kärt Kristina <kjurissa@tktk.ee>
2023-08-21 18:34:44 +0300

    Add last week

---
+++ Week15OOP/Program.cs
@@ -0,0 +1,51 @@
+﻿List<Movie> myMovies = new List<Movie>();
+string[] data = GetDataFromMyFiles();
+//ReadDataFromArray(data);
+
+foreach (string line in data)
+{
+    string[] tempArray = line.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+    Movie newMovie = new Movie(tempArray[0], tempArray[2]);
+    myMovies.Add(newMovie);
+}
+
+
+foreach (Movie movie in myMovies)
+{
+    Console.WriteLine($"One of my fav movies {movie.Title} was realeased in {movie.Year}");
+}
+
+static string[] GetDataFromMyFiles()
+{
+    string filePath = @"C:\Users\Kärt Kristina\Desktop\movies.txt";
+    return File.ReadAllLines(filePath);
+}
+
+static void ReadDataFromArray(string[] someArray)
+{
+    foreach (string line in someArray)
+    {
+        Console.WriteLine(line);
+    }
+}
+
+class Movie
+{
+    string title;
+    string year;
+    public string Title
+    {
+        get { return title; }
+    }
+
+    public string Year
+    {
+        get { return year; }
+    }
+
+    public Movie(string _title, string _year)
+    {
+        title = _title;
+        year = _year;
+    }
+}
